b544e8687ed31f9b369b2ac20880df07b8c1d6ec
Ziming Zhong <66154532+SeSeSeven@users.noreply.github.com>
2024-07-15 03:56:19 +0200

    Update run_project_commands.sh

--- run_project_commands.sh
+++ run_project_commands.sh
@@ -6,7 +6,7 @@ set -e
 python data.py
 python model.py
 python main.py train --lr 1e-4 --bucket_name "my_lmu_mlops_data_bucket"
-python main.py evaluate trained_model.pt
+python main.py evaluate trained_model.pt --bucket_name "my_lmu_mlops_data_bucket"
 
 # 保持容器运行（如果需要）
 # tail -f /dev/null
